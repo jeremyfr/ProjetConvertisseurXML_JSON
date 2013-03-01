3596065c789d408dcf52615f5c1170e339fb6649
clement larrieu <homzon@gmail.com>
2013-03-01 15:16:08 +0100

    premier essai xml->json #2

--- ProjetConvertisseurXML_JSON/src/main/java/dcll/jfri/projetConvertisseur/App.java
+++ ProjetConvertisseurXML_JSON/src/main/java/dcll/jfri/projetConvertisseur/App.java
@@ -1,8 +1,19 @@
 package dcll.jfri.projetConvertisseur;
 
+import org.omg.CORBA.portable.InputStream;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.sf.json.JSON;
+import net.sf.json.JSONSerializer;
 import net.sf.json.xml.XMLSerializer;
 
+import org.apache.commons.io.IOUtils;
 /**
  * Hello world!
  *
@@ -11,13 +22,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	XMLSerializer serializer = new XMLSerializer();
-    	serializer.setTypeHintsEnabled(false);
-
-    	JSON jsonRepresentation = serializer.readFromFile( "file.xml" );
-
-    	String xml = serializer.write( jsonRepresentation );
-    	System.out.println(xml);
+    	String xml = "<o number=\"1\"> first<string>json</string> <array>  <e>1</e><e>true</e></array> </o>";
+    	
+    	
+    	XMLSerializer xmlSerializer = new XMLSerializer();  
+    	JSON json = xmlSerializer.read( xml );  
+    	System.out.println( json.toString(2) );  
 
     }
 }
