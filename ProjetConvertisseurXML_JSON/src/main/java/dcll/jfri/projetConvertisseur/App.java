296b2d5fe32fa19ec8ef12120c861c6828044d0f
clement larrieu <homzon@gmail.com>
2013-03-01 17:33:09 +0100

    xml->json #2

--- ProjetConvertisseurXML_JSON/src/main/java/dcll/jfri/projetConvertisseur/App.java
+++ ProjetConvertisseurXML_JSON/src/main/java/dcll/jfri/projetConvertisseur/App.java
@@ -41,8 +41,7 @@ public class App
     	java.io.InputStream is = App.class.getResourceAsStream("testxml.txt");
     	String jsonData = IOUtils.toString(is);  */   
     	
-    	//String jsonData = recupStringFichier("C:\\Utilisateurs\\Helene\\Téléchargements\\quiz-moodle-exemple.xml");
-    	
+    
     	FileInputStream fichier=new FileInputStream("testxml.xml");
     	StringWriter writer=new StringWriter();
     	InputStreamReader streamReader=new InputStreamReader(fichier);
@@ -50,55 +49,43 @@ public class App
     	BufferedReader buffer=new BufferedReader(streamReader);
     	String line="";
     	while ( null!=(line=buffer.readLine())){
-    	writer.write(line);
+    	  writer.write(line);
     	}
     	// Sortie finale dans le String
     	String jsonData=writer.toString();
     	
     	System.out.println(jsonData);
     	
-    	
-    	
-    	//String xmlAconvertir = IOUtils.toString(isXML);
-    	
-    	//System.out.println( is );
-    	
     	XMLSerializer xmlSerializer = new XMLSerializer();
     	xmlSerializer.setTypeHintsEnabled( false ); 
     	JSON json = xmlSerializer.read( jsonData); 
     	
-    	System.out.println( json.toString(2) );  
+    	System.out.println( json.toString(2) );
     	
-    	//String str = json.toString(2);  
-    	//JSONObject json1 = (JSONObject) JSONSerializer.toJSON( str );
-    	JSONObject json1 = (JSONObject) json;
-    	XMLSerializer xmlSerializer1 = new XMLSerializer();  
-    	xmlSerializer1.setTypeHintsEnabled( false );  
+;
     	
-    	String xml1 = xmlSerializer1.write( json1 );  
-    	System.out.println( xml1 );  
+    	
+
+    	/*
+		String jsonData1 = jsonData;
+		
+		XMLSerializer serializer = new XMLSerializer();
+		serializer.setRootName("quiz");
+        serializer.setSkipWhitespace(false);
+        // Le type des objets ne seront pas affichÃ©s
+        serializer.setTypeHintsEnabled(false);
+        
+		JSON jsonAconvertir = JSONSerializer.toJSON(jsonData1);
+
+		 
+    	
+
+    	System.out.println( serializer.write(jsonAconvertir));  
     	/*json1.getJSONArray( "number" ).setExpandElements( true );  
     	xml1 = xmlSerializer1.write( json );  
     	System.out.println( xml1 );*/ 
 
     }
     
-    /*public static String recupStringFichier(String nomFichier) {
-    	String retour;
-    try{
-    	File f = new File(nomFichier);
-        byte[] buffer = new byte[(int)f.length()];
-        DataInputStream in = new DataInputStream(new FileInputStream(f));
-        in.readFully(buffer);
-        in.close();
-    	retour = new String(buffer);
-            return retour;
-        } catch (FileNotFoundException e) {
-            System.out.println("Impossible de lire le fichier "+nomFichier+" ! " +e);
-            return "";
-        } catch (IOException e) {
-        	System.out.println("Erreur de lecture !" +e);
-        	return "";
-        }
-    }*/
+  
 }
