b9ef847acf4e0b656eb3c2fcc2cc0fdd00d8dfdd
clement larrieu <homzon@gmail.com>
2013-03-01 15:38:32 +0100

    premier essai json->xml #2

--- ProjetConvertisseurXML_JSON/src/main/java/dcll/jfri/projetConvertisseur/App.java
+++ ProjetConvertisseurXML_JSON/src/main/java/dcll/jfri/projetConvertisseur/App.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.sf.json.JSON;
+import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 import net.sf.json.xml.XMLSerializer;
 
@@ -23,11 +24,25 @@ public class App
     public static void main( String[] args )
     {
     	String xml = "<o number=\"1\"> first<string>json</string> <array>  <e>1</e><e>true</e></array> </o>";
+    	System.out.println( xml );
+    	InputStream isXML = (InputStream) App.class.getResourceAsStream("testxml.xml");
+    	XMLSerializer xmlSerializer = new XMLSerializer();
+    	xmlSerializer.setTypeHintsEnabled( false ); 
+    	JSON json = xmlSerializer.read( xml ); 
     	
-    	
-    	XMLSerializer xmlSerializer = new XMLSerializer();  
-    	JSON json = xmlSerializer.read( xml );  
     	System.out.println( json.toString(2) );  
+    	
+    	//String str = json.toString(2);  
+    	//JSONObject json1 = (JSONObject) JSONSerializer.toJSON( str );
+    	JSONObject json1 = (JSONObject) json;
+    	XMLSerializer xmlSerializer1 = new XMLSerializer();  
+    	xmlSerializer1.setTypeHintsEnabled( false );  
+    	
+    	String xml1 = xmlSerializer1.write( json1 );  
+    	System.out.println( xml1 );  
+    	/*json1.getJSONArray( "number" ).setExpandElements( true );  
+    	xml1 = xmlSerializer1.write( json );  
+    	System.out.println( xml1 );*/  
 
     }
 }
